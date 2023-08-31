31ef570e2505f6de66e12faac06d3d9e5211a91a
91112021 <dalebolido@gmail.com>
2023-08-31 13:20:52 -0700

    Created a Karen() object

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,21 @@ public class Main {
     {
         //instantiate objects
         Dragon spyro = new Dragon("Spyro", "Dragon", "Fire", 1227, 44.0, 2, 8);
-        Dragon Toothless = new Dragon("Mushu", "Dragon", "Dark", 60, 1776.0, 2, 10);
+        Dragon toothless = new Dragon("Toothless", "Dragon", "Dark", 60, 1776.0, 2, 10);
 
         Griffin peter = new Griffin("Peter", "Griffin", "Normal", 768, 220.0, 10, 40);
         Griffin bob = new Griffin("Bob", "Griffin", "Normal", 72, 500.0, 2, 20);
 
         Unicorn tspark = new Unicorn("Twilight Sparkle", "Unicorn", "Fairy", 300, 181.0, 10, 1000);
         Unicorn bstall = new Unicorn("Butt Stallion", "Binicorn", "Fairy", 100, 78.0, 20, 10);
+
+        Karen karen = new Karen("Karen", 6);
+        karen.addRacer(spyro);
+        karen.addRacer(toothless);
+        karen.addRacer(peter);
+        karen.addRacer(bob);
+        karen.addRacer(tspark);
+        karen.addRacer(bstall);
 
 
 
