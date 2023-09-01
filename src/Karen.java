fdb34d4ef88b53d5b6139e864f7f6922367dbac6
DaleB34 <dalebolido@gmail.com>
2023-08-31 20:50:05 -0700

    first animal for start() works

--- src/Karen.java
+++ src/Karen.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Karen
 {
     //mostly going off base structure of Farmer from Old MacDonald
@@ -11,19 +12,50 @@ public class Karen
         racers = new MagicAnimal[numRacers];
     }
 
+    //brain methods
+
+    public void start()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Here are all the racers you can choose from: " + toString());
+
+        //select 1st animal
+        System.out.println("Please enter the first number of the animal you want to race: ");
+        int a1 = sc.nextInt();
+        while(a1 < 1 || a1 > racers.length)
+        {
+            System.out.println("Invalid Answer\nPlease enter the first number of the animal you want to race:");
+            a1 = sc.nextInt();
+        }
+
+
+
+    }
+
+    public void race(MagicAnimal a1, MagicAnimal a2)
+    {
+
+
+
+    }
+
     public void addRacer(MagicAnimal a)
     {
         racers[count] = a;
         count++;
     }
 
+
+    //toString
     public String toString()
     {
         //print data of all racers
         String output = "";
+        int i = 1;
         for(MagicAnimal a : racers)
         {
-            output+= "\n" + a + "\n"; //formatting yayayayay
+            output+= "\n" + i + ". " + a + "\n"; //formatting yayayayay
+            i++;
         }
         return output;
 
